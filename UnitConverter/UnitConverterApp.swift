188506fbe022839d18d22f1f2d5ef810e3f4ed17
Fiona Voss <me@fionavoss.com>
2024-11-18 10:27:50 -0800

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Fiona Voss on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UnitConverterTests/UnitConverterTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterTests.swift
+//  UnitConverterTests
+//
+//  Created by Fiona Voss on 11/18/24.
+//
+
+import Testing
+@testable import UnitConverter
+
+struct UnitConverterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
