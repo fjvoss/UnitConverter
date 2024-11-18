98b8fae2c2f3b6478d641cca6b0ea02a6b9c102a
Fiona Voss <me@fionavoss.com>
2024-11-18 10:58:17 -0800

    Basic form UI

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    let unitOptions = ["Celsius", "Fahrenheit", "Kelvin"]
+    
+    @State private var inputUnit = "Celsius"
+    @State private var outputUnit = "Fahrenheit"
+    @State private var inputValue = 100
+    
+    var temperatureInCelsius: Int {
+        switch inputUnit {
+        case "Celsius":
+            return inputValue
+        case "Fahrenheit":
+            return inputValue // todo
+        case "Kelvin":
+            return inputValue // todo
+        default:
+            return inputValue // todo
         }
-        .padding()
+    }
+    
+    var temperatureInSelectedUnits: Int {
+        switch outputUnit {
+        case "Celsius":
+            return temperatureInCelsius
+        case "Fahrenheit":
+            return temperatureInCelsius // todo
+        case "Kelvin":
+            return temperatureInCelsius // todo
+        default:
+            return temperatureInCelsius // todo
+        }
+    }
+    
+    var body: some View {
+        Form {
+            Section("Input unit") {
+                Picker("Input unit", selection: $inputUnit) {
+                    ForEach(unitOptions, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Output unit") {
+                Picker("Output unit", selection: $outputUnit) {
+                    ForEach(unitOptions, id: \.self) {
+                        Text($0)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            
+            Section("Value") {
+                TextField("Amount", value: $inputValue, format: .number)
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section("Result") {
+                Text(temperatureInSelectedUnits, format: .number)
+            }
+        }
     }
 }
 
